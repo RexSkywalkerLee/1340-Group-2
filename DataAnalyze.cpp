76116d66cb69b4d709e291f3b52ad04f6e20dd6c
joyce71 <43691213+joyce71@users.noreply.github.com>
2019-04-20 12:17:11 +0800

    develop a standard method to analyze data

---
+++ DataAnalyze.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include"DataAnalyze.h"
+
+using namespace std;
---
+++ DataAnalyze.h
@@ -0,0 +1,15 @@
+#ifndef head_h
+#define head_h
+#include<string>
+using namespace std;
+
+class Data{
+public:
+  void retrieve();
+  void sum();
+
+private:
+  string user;
+};
+
+#endif
